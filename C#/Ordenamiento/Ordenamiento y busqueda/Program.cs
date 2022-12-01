ef4a787cbb001b4c9e97bac25d33b6bb019527c5
Nicolas Vitek <71182488+NicolasVitek@users.noreply.github.com>
2022-12-01 14:11:00 -0300

    Add files via upload

---
+++ C#/Ordenamiento/Ordenamiento y busqueda/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordenamiento_y_busqueda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola");
+            Console.ReadKey(true);
+        }
+    }
+}
