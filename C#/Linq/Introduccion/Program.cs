850e613670dfc87a41378478981706929035222e
Nicolas Vitek <71182488+NicolasVitek@users.noreply.github.com>
2022-12-01 14:07:34 -0300

    Add files via upload

---
+++ C#/Linq/Introduccion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = {1, 2, 3, 4, 5};
+            //QUERY
+            IEnumerable<int> valores = from n in numeros
+                                       where n > 3 && n < 8
+                                       select n;
+            //MOSTRAMOS LOS RESULTADOS
+            foreach (int item in valores)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            string[] postres = { "helado", "sandia", "pastel de manzana", "ensala de frutas", "manzana de caramelo"};
+
+            IEnumerable<string> encontrados = from p in postres
+                                              where p.Contains("manzana")
+                                              orderby p
+                                              select p;
+            foreach (string item in encontrados)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            //HACEMOS UNA REFLEXION
+            informacionResultados(valores);
+            Console.WriteLine();
+            informacionResultados(encontrados);
+        }
+        static void informacionResultados(object resultados)
+        {
+            Console.WriteLine("Tipo {0}", resultados.GetType().Name);
+            Console.WriteLine("Locacion {0}", resultados.GetType().Assembly.GetName().Name);
+        }
+    }
+}
---
+++ C#/Linq/Query progresivo/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query_progresivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //QUERY EN VARIOS PASOS PARA METER LOGICA ANTES DEL RESULTADO FINAL
+            string[] postres = { "helado", "sandia", "pastel de manzana", "ensala de frutas", "manzana de caramelo" };
+            bool mayusculas = false;
+            IEnumerable<string> resultado;
+
+            var manzanas = postres.Where(n => n.Contains("manzana"));
+            var ordenadas = manzanas.OrderBy(n => n);//ORDEN ALFABETICO
+            if (mayusculas)
+            {
+                resultado = ordenadas.Select(n => n.ToUpper());
+            }
+            else
+            {
+                resultado = ordenadas;
+            }
+            foreach (string item in resultado)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            //INTO ES UNA CONTINUACION DE LA COSNSULTA: LA REINICIA PERMITIENDO OTROS WHERE, SELECT U ORDERBY
+            //SOLO SE PUEDE USAR DESPUES DE SELECT O GROUP
+            IEnumerable<string> encontrados = from p in postres
+                                              where p.Contains("manzana")
+                                              orderby p
+                                              select p
+                                              into pays
+                                              where pays.Contains("tarta")
+                                              select pays;
+            foreach (string item in encontrados)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            //ENVOLVER CONSULTAS CON WRAPPING
+            IEnumerable<string> mipay = from p in
+                                            (
+                                            from p1 in postres
+                                            where p1.Contains("manzana")
+                                            orderby p1
+                                            select p1
+                                            )
+                                        where p.Contains("tarta")
+                                        select p;
+            foreach (string item in mipay)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            //LET SE USA PARA COLOCAR UNA NUEVA VARIABLE JUNTO CON LA DE RANGO
+            IEnumerable<string> mipay2 = from p in postres
+                                let manzanitas =(
+                                from p1 in postres
+                                where p1.Contains("manzana")
+                                orderby p1
+                                select p1
+                                )
+                                        where manzanitas.Contains("tarta")
+                                        select p;
+            foreach (string item in mipay2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
