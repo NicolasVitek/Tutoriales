26d9bb7398d48020e59b20a13a0f23836d331c39
Nicolas Vitek <71182488+NicolasVitek@users.noreply.github.com>
2022-12-06 20:10:59 -0300

    Add files via upload

---
+++ Windows forms/Date Time/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Date_Time
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOperaciones_Click(object sender, EventArgs e)
+        {
+            DateTime fecha = DateTime.Today;
+            string formato = "MMM ddd d HH mm yyyy";
+            lblFecha.Text = fecha.ToString(formato);
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime fecha = dateTimePicker1.Value;
+            lblDate.Text = fecha.ToString();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double dia = Convert.ToDouble(txtDias.Text);
+            dateTimePicker1.Value = DateTime.Today.AddDays(dia);
+        }
+
+        private void lblDate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
